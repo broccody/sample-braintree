72eb9a0a8e1c79f20cf702e8b59fbf30765768d0
Boonyarit Eoyeye <yim.boonyarit@gmail.com>
2019-12-13 16:39:42 +0700

    edit config

--- config/config.js
+++ config/config.js
@@ -6,12 +6,24 @@ const braintree = require('braintree');
 
 export const AppConfig = {
     environment: 'sandbox',
-    availablePlan:['hp2w'],
+    availablePlan:[
+        'free',
+        'free_tier',
+        'monthly_small_business_1',
+        'monthly_small_business_2',
+        'monthly_small_business_3',
+        'monthly_middle_business_1',
+        'monthly_middle_business_2',
+        'monthly_middle_business_3',
+        'monthly_large_business_1',
+        'monthly_large_business_2',
+        'monthly_large_business_3',
+    ],
     enableDebug: true,
     sandbox: {
         environment: braintree.Environment.Sandbox,
-        merchantId: 'z23jqghp8ysp54dg',
-        publicKey: 'b4v82hg9rkg8246m',
-        privateKey: '5e1882cba82ecc35f8ef42f34da9710d'
+        merchantId: 'httchtvjtmk8q6z3',
+        publicKey: 'sh8588ds4btxrqcb',
+        privateKey: '05a3384e1c5414f622d0a80573bfea7a'
     }
 }
